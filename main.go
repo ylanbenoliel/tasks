b1310bf920b6d267138c735a214983ec550800d4
Ylan Benoliel <ylanbenoliel@gmail.com>
2025-08-30 01:02:22 -0300

    List tasks

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"text/tabwriter"
 	"time"
 )
 
@@ -19,8 +20,12 @@ type Task struct {
 }
 
 func main() {
-	var message string
+	var (
+		message string
+		list    bool
+	)
 	flag.StringVar(&message, "a", "", "Create new task")
+	flag.BoolVar(&list, "l", false, "List tasks")
 	flag.Parse()
 
 	file, err := OpenDatabase()
@@ -33,6 +38,14 @@ func main() {
 		err = addTask(file, message)
 		if err != nil {
 			log.Fatalf("Error adding new task %v", err)
+		}
+		fmt.Print("New task saved!")
+	}
+
+	if list {
+		err = listTasks(file)
+		if err != nil {
+			log.Fatalf("Error task list %v", err)
 		}
 	}
 }
@@ -97,5 +110,47 @@ func addTask(database *os.File, message string) error {
 	if err := writer.Error(); err != nil {
 		return fmt.Errorf("error writing new task %v", err)
 	}
+	return nil
+}
+
+func listTasks(database *os.File) error {
+	// Positioning cursor to start of file
+	_, err := database.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("error seeking to start of file %v", err)
+	}
+
+	var lines [][]string
+	reader := csv.NewReader(database)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("error reading csv %v", err)
+		}
+		lines = append(lines, record)
+	}
+
+	writer := tabwriter.NewWriter(
+		os.Stdout, 0, 2, 4, ' ', 0,
+	)
+
+	writer.Write(
+		[]byte("ID\tTask\tCreated\tDone\n"),
+	)
+
+	for _, line := range lines {
+		writer.Write(
+			[]byte(fmt.Sprintf("%v\t%v\t%v\t%v\n", line[0], line[1], line[2], line[3])),
+		)
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return fmt.Errorf("error getting tasks %v", err)
+	}
+
 	return nil
 }
